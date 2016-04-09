52019f227389fed04eeb38c1d8a32f1544d81b95
Piruz <quest.uasphere@gmail.com>
2016-04-10 01:24:48 +0200

    Codacy: Eliminando imports que no usamos
    Codacy

--- src/test/java/es/uniovi/asw/selenium/UnioviTest.java
+++ src/test/java/es/uniovi/asw/selenium/UnioviTest.java
@@ -1,13 +1,13 @@
 package es.uniovi.asw.selenium;
 
-import java.util.regex.Pattern;
+//import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+//import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+//import org.openqa.selenium.support.ui.Select;
 
 public class UnioviTest {
   private WebDriver driver;
