5f620d3da1180e74b0e558c797a77c99aa95fa28
davidglezz <davidglezz@outlook.es>
2016-04-14 00:55:40 +0200

    Navegación simple

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,14 +3,36 @@
  * */
 
 $(document).ready(function() {
+	
+	// Rutas
+	page('/', index);
+	page('/help', help);
+	page('/elecciones', list);
+	page('/elecciones/:eleccion', eleccion);
+	page('*', notfound);
 
 	page();
 
 });
 
-page('/', index);
-page('/user/list', list);
-page('/user/help', help);
-page('/user/:eleccion', album);
-page('/user/:user/album/sort', sort);
-page('*', notfound);
+var index = function () {
+	
+}
+
+var help = function () {
+	
+}
+
+var list = function () {
+}
+
+var eleccion = function () {
+	
+}
+
+var notfound = function () {
+	$("#main > .active").removeClass("active");
+	$("#notfound").addClass("active");
+}
+
+
