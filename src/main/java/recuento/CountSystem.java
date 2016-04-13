92e5d884828013cb0983ce480bc5b6da1fc5aabc
davidglezz <davidglezz@outlook.es>
2016-04-13 23:37:47 +0200

    Base recuento

---
+++ src/main/java/recuento/CountSystem.java
@@ -0,0 +1,18 @@
+package recuento;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface CountSystem {
+
+	public boolean iniciar();
+
+	public boolean terminar();
+	
+	public HashMap<String, Long> getResults();
+	
+	public String getName();
+	
+	public List<String> GetOptions();
+	
+}
