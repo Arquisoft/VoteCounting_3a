6088b7f4d312392c75c81e783661ec04b8c3f32f
raulbt <raulbarba23@gmail.com>
2016-04-16 17:55:43 +0200

    Recuento Terminado

--- src/main/java/es/uniovi/asw/recuento/BeanCalculoVotos.java
+++ src/main/java/es/uniovi/asw/recuento/BeanCalculoVotos.java
@@ -5,24 +5,36 @@ import java.util.Map;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import es.uniovi.asw.model.Candidatura;
-import es.uniovi.asw.model.Eleccion;
-
 @Component("BeanCalculo")
 @Scope("session")
 public class BeanCalculoVotos {
 	
-	private Map<String, Integer> votos;
-	//private Map<String, Map<String, Integer>> colegios;
-	//private Map<String, Map<String, Integer>> comunidad;
+	private Map<String, Long> votos;
+	private Map<String, Map<String, Long>> colegios;
+	private Map<String, Map<String, Long>> comunidad;
 
 
-	public String recuentoVotos (Eleccion e) {
-		for(Candidatura cand : e.getOpciones()){
-			votos.put(cand.getNombre(), cand.getVotos().size());
-		}
+	public String recuentoVotos (CountSystem e) {
+		votos = e.getVotos();
+		colegios = e.getColegios();
+		comunidad = e.getComunidades();
 		
 		return "exito";
+	}
+
+
+	public Map<String, Long> getVotos() {
+		return votos;
+	}
+
+
+	public Map<String, Map<String, Long>> getColegios() {
+		return colegios;
+	}
+
+
+	public Map<String, Map<String, Long>> getComunidad() {
+		return comunidad;
 	}
 
 }
