c5ba8f9b30bdbab38b496ff3b63c9b568196190f
raulbt <raulbarba23@gmail.com>
2016-04-16 15:33:10 +0200

    Beans de obtener votos

--- src/main/java/es/uniovi/asw/recuento/BeanCalculoVotos.java
+++ src/main/java/es/uniovi/asw/recuento/BeanCalculoVotos.java
@@ -1,5 +1,28 @@
 package es.uniovi.asw.recuento;
 
+import java.util.Map;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import es.uniovi.asw.model.Candidatura;
+import es.uniovi.asw.model.Eleccion;
+
+@Component("BeanCalculo")
+@Scope("session")
 public class BeanCalculoVotos {
+	
+	private Map<String, Integer> votos;
+	//private Map<String, Map<String, Integer>> colegios;
+	//private Map<String, Map<String, Integer>> comunidad;
+
+
+	public String recuentoVotos (Eleccion e) {
+		for(Candidatura cand : e.getOpciones()){
+			votos.put(cand.getNombre(), cand.getVotos().size());
+		}
+		
+		return "exito";
+	}
 
 }
