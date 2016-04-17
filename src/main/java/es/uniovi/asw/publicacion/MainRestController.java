05e7a90f6b260fe56eda730fbbee2453857803b0
davidglezz <davidglezz@outlook.es>
2016-04-18 01:09:07 +0200

    SAVEPOINT

--- src/main/java/es/uniovi/asw/publicacion/MainRestController.java
+++ src/main/java/es/uniovi/asw/publicacion/MainRestController.java
@@ -1,19 +1,32 @@
 package es.uniovi.asw.publicacion;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import es.uniovi.asw.model.Eleccion;
+import es.uniovi.asw.persistence.Repository;
+
 @RestController
 public class MainRestController {
 
-	@RequestMapping(value = "/resultados/{id}",
+	@RequestMapping(value = "/elecciones",
+			method = RequestMethod.GET,
+			produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
+			consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	public ResponseEntity<List<Eleccion>> GetListOfElecciones() throws Exception {
+		System.out.println("/elecciones");
+		List<Eleccion> elecciones = (List<Eleccion>) Repository.eleccionRep.findAll();
+		return  new ResponseEntity<List<Eleccion>>(elecciones, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/eleccion/{id}",
 			method = RequestMethod.GET,
 			produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
 			consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
