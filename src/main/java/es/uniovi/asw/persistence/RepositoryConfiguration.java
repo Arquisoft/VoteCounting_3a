520e24b26fc0b6ba1fec25426aec1f186fd438f5
Palaskan <onecarlostlol@gmail.com>
2016-04-18 21:00:23 +0200

    Añadidos a los metodos de RepositoryConfiguration el codigo para añadir los datos del modelo y que se linkeen entre ellos

--- src/main/java/es/uniovi/asw/persistence/RepositoryConfiguration.java
+++ src/main/java/es/uniovi/asw/persistence/RepositoryConfiguration.java
@@ -1,11 +1,16 @@
 package es.uniovi.asw.persistence;
 
+import java.sql.Time;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import es.uniovi.asw.model.*;
 
 @Configuration
 @EnableAutoConfiguration
@@ -23,17 +28,32 @@ public class RepositoryConfiguration {
 	public static VotoRepository votoRep;
 	public static VotoConfirmadoRepository votoConfirmadoRep;*/
 	ComunidadAutonoma ca = new ComunidadAutonoma("Asturias");
+	Circunscripcion c1 = new Circunscripcion("Gijon", ca);
+	Circunscripcion c2 = new Circunscripcion("Oviedo", ca);
+	Circunscripcion c3 = new Circunscripcion("Aviles", ca);
+	Circunscripcion c4 = new Circunscripcion("Mieres", ca);
 	Eleccion e = new Eleccion("EleccionASW", new Date(), 
-			new Date("01/01/2017"), new Time(System.currentTimeMillis()), 
+			new Date(System.currentTimeMillis()+86400000), new Time(System.currentTimeMillis()), 
 			new Time(System.currentTimeMillis()+100000));
+	Candidatura can1 = new Candidatura("PP","Partido Popular","ti tiri tiririririi", e );
+	Candidatura can2 = new Candidatura("PSOE","Partido Socialista","zz", e );
+	Candidatura can3 = new Candidatura("Podemos","Partido de Podemos","Presidente con coleta", e );
+	ColegioElectoral colegio1 = new ColegioElectoral("Colegio de Gijon", "Poblacion gijonesa", c1);
+	ColegioElectoral colegio2 = new ColegioElectoral("Colegio de Oviedo", "Poblacion ovetense", c2);
+	Voter v1 = new  Voter("Carlos", "email1@uniovi.es", "pass1", "7895176D");
+	Voter v2 = new  Voter("Raul", "email2@uniovi.es", "pass2", "98751487D");
+	Voter v3 = new  Voter("Amir", "email3@uniovi.es", "pass3", "1234567B");
+	Voter v4 = new  Voter("David", "email4@uniovi.es", "pass4", "8747414D");
+	Voto voto = new Voto(colegio1);
+	
 	
 	@Autowired
 	public void setCandidaturaRep(CandidaturaRepository candidaturaRep){
 		Repository.candidaturaRep = candidaturaRep;
 		if(candidaturaRep.count() == 0){
-			candidaturaRep.save(new Candidatura("PP","Partido Popular","ti tiri tiririririi", e ));
-			candidaturaRep.save(new Candidatura("PSOE","Partido Socialista","zz", e ));
-			candidaturaRep.save(new Candidatura("Podemos","Partido de Podemos","Presidente con coleta", e ));
+			candidaturaRep.save(can1);
+			candidaturaRep.save(can2);
+			candidaturaRep.save(can3);
 		}
 	}
 	
@@ -42,23 +62,27 @@ public class RepositoryConfiguration {
 		Repository.circunscripcionRep = circunscripcionRep;
 		
 		if (circunscripcionRep.count() == 0) {
-			circunscripcionRep.save(new Circunscripcion("Gijon", ca));
-			circunscripcionRep.save(new Circunscripcion("Oviedo", ca));
-			circunscripcionRep.save(new Circunscripcion("Aviles", ca));
-			circunscripcionRep.save(new Circunscripcion("Mieres", ca));
+			circunscripcionRep.save(c1);
+			circunscripcionRep.save(c2);
+			circunscripcionRep.save(c3);
+			circunscripcionRep.save(c4);
 		}
 	}
 	
 	@Autowired
 	public void setColegioRep(ColegioRepository colegioRep){
 		Repository.colegioRep = colegioRep;
+		if(colegioRep.count()==0){
+			colegioRep.save(colegio1);
+			colegioRep.save(colegio2);
+		}
 	}
 	
 	@Autowired
 	public void setComunidadRep(ComunidadRepository comunidadRep){
 		Repository.comunidadRep = comunidadRep;
 		if (comunidadRep.count() == 0) {
-			ComunidadRepository.save(ca);
+			comunidadRep.save(ca);
 		}
 	}
 	
@@ -66,7 +90,7 @@ public class RepositoryConfiguration {
 	public void setEleccionRep(EleccionRepository eleccionRep){
 		Repository.eleccionRep = eleccionRep;
 		if (eleccionRep.count() == 0) {
-			EleccionRepository.save(e);
+			eleccionRep.save(e);
 		}
 	}
 	
@@ -74,21 +98,27 @@ public class RepositoryConfiguration {
 	public void setVoterRep(VoterRepository voterRep){
 		Repository.voterRep = voterRep;
 		if (voterRep.count() == 0) {
-			VoterRepository.save(new  Voter("Carlos", "email1@uniovi.es", "pass1", "7895176D"));
-			VoterRepository.save(new  Voter("Raul", "email2@uniovi.es", "pass2", "98751487D"));
-			VoterRepository.save(new  Voter("Amir", "email3@uniovi.es", "pass3", "1234567B"));
-			VoterRepository.save(new  Voter("David", "email4@uniovi.es", "pass4", "8747414D"));
+			voterRep.save(v1);
+			voterRep.save(v2);
+			voterRep.save(v3);
+			voterRep.save(v4);
 		}
 	}
 	
 	@Autowired
 	public void setVotoRep(VotoRepository votoRep){
 		Repository.votoRep = votoRep;
+		if(votoRep.count() == 0){
+			votoRep.save(voto);
+		}
 	}
 	
 	@Autowired
 	public void setVotoConfirmadoRep(VotoConfirmadoRepository votoConfirmadoRep){
 		Repository.votoConfirmadoRep = votoConfirmadoRep;
+		if(votoConfirmadoRep.count() == 0){
+			votoConfirmadoRep.save(new VotoConfirmado(v2, e));
+		}
 	}
 
 }
