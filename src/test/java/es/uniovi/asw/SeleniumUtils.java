b7bde0fdd76b55f6bf6140bd4314d250f9972251
raulbt <raulbarba23@gmail.com>
2016-04-20 23:45:03 +0200

    Selenium remoto utils

--- src/test/java/es/uniovi/asw/SeleniumUtils.java
+++ src/test/java/es/uniovi/asw/SeleniumUtils.java
@@ -1,6 +1,8 @@
 package es.uniovi.asw;
 
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -8,11 +10,54 @@ import static org.junit.Assert.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.saucelabs.saucerest.SauceREST;
+
 public class SeleniumUtils {
+
+	public static WebDriver getDriver(String testName){
+		WebDriver driver;
+
+		if(System.getenv().get("TRAVIS_JOB_NUMBER") != null){
+
+			URL url = null;
+			try {
+				url = new URL("http://" + System.getenv().get("SAUCE_USERNAME") + ":" 
+						+ System.getenv().get("SAUCE_ACCESS_KEY") + "@ondemand.saucelabs.com/wd/hub");
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+
+			DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+			capabilities.setCapability("tunnel-identifier", System.getenv().get("TRAVIS_JOB_NUMBER"));
+			capabilities.setCapability("name", testName);
+
+			driver = new RemoteWebDriver(url, capabilities);
+
+		}
+		else{
+			driver = new FirefoxDriver();
+		}
+
+		return driver;
+	}
+
+	public static void finishTest(WebDriver driver){
+		if (System.getenv().get("TRAVIS_JOB_NUMBER") != null) {
+			SauceREST sauceRest = new SauceREST(System.getenv().get("SAUCE_USERNAME"), System.getenv().get("SAUCE_ACCESS_KEY"));
+			sauceRest.jobPassed((((RemoteWebDriver) driver).getSessionId()).toString());
+		}
+
+		driver.close();
+		driver.quit();
+	}
+
 
 	//Mueve el ratón a la opción de menú submenu(desplegable). Evento hover
 	//y clicka la opcion opcionclick
